7a15d2d271ea4b3f4f10499649734226aff4fe9d
katieburrows <katie_burrows@me.com>
2019-03-27 14:46:02 -0600

    added click event, created questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,52 @@
-console.log("hi");
+//the app starts with a blank page and a start button as the only thing on the page
+    //click event on start to:
+        //the start button is wiped away and the quiz starts    
+            //timer is counting down
+                //display countdown
+            //all the questions are displayed (10 questions)
+            //all of the possible answers are displayed
+                //make sure the user can only check one radio button
+            //once the timer is done OR the user has hit a submit button the screen is wiped away and the score is reported
+                //correct, incorrect, unanswered.
+$("#start").on("click", function(event){
+    event.preventDefault();
+    $("#start").hide();
+
+
+});
+
+
+//trivia questions:
+
+/*
+"Alexander Hamilton was a US President."
+    "False"
+
+"There are four seasons every year."
+    "True"
+
+"10 + 10 = 200."
+    "False"
+
+"There are 6 continents."
+    "False"
+
+"During the US's summer world-wide carbon emissions are the lowest."
+    "True"
+
+"I-25 is a literal nightmare to drive."
+    "True"
+
+"Vanilla JavaScript is comprised of jQuery and React."
+    "False"
+
+"There are 6 primary colors."
+    "False"
+
+"H20 is denser in its liquid state than its solid state."
+    "True"
+
+"New Mexico is one of the states that comprise 'the Four Corners.'"
+    "True"
+
+*/
