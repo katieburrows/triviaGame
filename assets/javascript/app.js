b542e972fdd612c37c2331b33ba1f688d65c3e65
katieburrows <katie_burrows@me.com>
2019-06-28 22:58:15 -0600

    new questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-            //all of the possible answers are displayed w/ radio button
-                //make sure the user can only check one radio button
-            //once the timer is done OR the user has hit a submit button the screen is wiped away and the score is reported
-                //correct, incorrect, unanswered.
 var timer = 3;
 var intervalId;
 var right = 0;
@@ -9,21 +5,22 @@ var wrong = 0;
 var noAnswer = 0;
 
 var questions = [{ 
-    question: "1. Alexander Hamilton was a US President.",
-    answers: ["true", "false"],
-    correctAnswer: "false"
+    question: "1. What animal does Professor McGonagall turn into?",
+    answers: ["toad", "rat", "cat", "penguin"],
+    correctAnswer: "cat",
+    image: ""
 },  {
-    question: "2. There are four seasons every year.",
-    answers: ["true", "false"],
-    correctAnswer: "true"
+    question: "2. What Magical Creature has Hagrid always wanted?",
+    answers: ["three headed dog", "dragon", "boarhound", "blast ended skrewt" ],
+    correctAnswer: "dragon"
 },  {
-    question: "3. 10 + 10 = 200.",
-    answers: ["true", "false"],
-    correctAnswer: "false"
+    question: "3. How many horcurxes in total did Lord Voldemort make?",
+    answers: ["6", "7", "8", "9"],
+    correctAnswer: "8"
 },  {
-    question: "4. There are 6 continents.",
-    answers: ["true", "false"],
-    correctAnswer: "false"
+    question: "4. Who is Harry Potters Godfather",
+    answers: ["Peter Pettigrew", "Sirius Black", "Remus Lupin", "Severus Snape"],
+    correctAnswer: "Sirius Black"
 }, {
     question: "5. I-25 is a literal nightmare to drive.",
     answers: ["true", "false"],
@@ -53,7 +50,8 @@ var questions = [{
 //when the start button is clicked it kicks off our code.
 $("#start").on("click", function(event){
     event.preventDefault();
-   //hiding the start button.
+   
+    //hiding the start button.
     $("#start").hide();
 
     //function to decrement counter
@@ -61,6 +59,7 @@ $("#start").on("click", function(event){
 
     //looping through all the questions
     for (var i = 0; i < questions.length; i++){
+
         //creating a new paragraph for each question.
         var newP = $("<p>");
         
@@ -75,9 +74,22 @@ $("#start").on("click", function(event){
         $("#questionDisplay").append(newP);
         
         //appending radio buttons onto the page
-        $("#questionDisplay").append(radioButtonTrue, radioButtonFalse);
-        
+        $("#questionDisplay").append(radioButtonTrue, radioButtonFalse);   
     }
+
+    //creating submit button using jQuery to apper once the start button has been hit.
+    $("#submitButtonSpace").append("<button id = 'submit'>Submit it!</button>");
+
+    //click event for the start button
+    $("#submit").on("click", function(){
+        //taking the timer off the page.
+        $("#countDown").hide();
+
+        $("#gameArea").hide();
+        
+        //triggering the function that will check the answers.
+        calculateScore();
+    })
 });
 
 function counter(){
@@ -105,10 +117,36 @@ function decrement() {
         }
 }
 
-function calculateScore(){
-    console.log("calculate score function hit");
+if (timer === 0) {
+    //taking the timer off the page.
+    $("#countDown").hide();
 
+    $("#gameArea").hide();
     
+    //triggering the function that will check the answers.
+    calculateScore();
+}
+
+function calculateScore(){
+//thinking that $(this).attr() will be used and looped through again to check against the grading key 
+
+            //if value grab the value and store it in userGuess
+            //grab value of radio button
+            //compare userGuess to questions[i].answers
+
+//two ways to go about this potentially:  create a userGuess variable that will be able to tell if there is no value stored in it (not sure if this is possible with forms, probably is with :checked) OR if there is a true/false value in it.  Use that in the conditionals, if that doesn't work then may need to a create a variable once inside the conditional section that grabs the value (so 2-3 instances of this variable to be created/mutated).
+    
+    for (var i = 0; i < questions[i].answers.length; i++){
+        var radioValue = $(this).attr();
+        console.log(radioValue);
+    }
+        // var radioValue = $("input[name='question-" + [i] + "']:checked").val();
+        // if(radioValue){
+        //     console.log(`${"radio value:"} ${radioValue}`);
+        // }
+  
+    
+
     //check if there was an answer; compare userGuess of radio button to correct answer to calculate the score.
         /*if (!value){
             noAnswer++
@@ -125,22 +163,9 @@ function calculateScore(){
         $("#scoreBoard").append(`${"Number of wrong answers:"} ${wrong}`);
         $("#scoreBoard").append(`${"Number of unanswered questions:"} ${noAnswer}`);
         */
-
-
-        //if no value unanswered++
-        //if value grab the value and store it in userGuess
-            //compare userGuess to questions[i].answers
-                //if correct correctAnswers++
-                //if wrong wrongAnswers++
-                $( "label" ).on( "click", function() {
-                    console.log( $( "label:checked" ).val() + " is checked!" );
-                  });
         
-    //thinking that $(this).val() will be used and looped through again to check against the grading key that's been set up with data-.
-
-    
-        //grab value of radio button
 }
 
+//once the timer is done OR the user has hit a submit button the screen is wiped away and the score is reported
 
 //use data- ?
