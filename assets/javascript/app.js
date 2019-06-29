600ec1ab3251ec3b9021405a05f88d3326d85687
katieburrows <katie_burrows@me.com>
2019-06-28 23:36:07 -0600

    added gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var timer = 3;
+var timer = 30;
 var intervalId;
 var right = 0;
 var wrong = 0;
@@ -8,43 +8,52 @@ var questions = [{
     question: "1. What animal does Professor McGonagall turn into?",
     answers: ["toad", "rat", "cat", "penguin"],
     correctAnswer: "cat",
-    image: ""
+    image: "https://giphy.com/embed/y3Z92OG7KPCXS"
 },  {
     question: "2. What Magical Creature has Hagrid always wanted?",
     answers: ["three headed dog", "dragon", "boarhound", "blast ended skrewt" ],
-    correctAnswer: "dragon"
+    correctAnswer: "dragon",
+    image: "https://media.giphy.com/media/11EHYMlR23cji8/giphy.gif"
 },  {
     question: "3. How many horcurxes in total did Lord Voldemort make?",
     answers: ["6", "7", "8", "9"],
-    correctAnswer: "8"
+    correctAnswer: "8",
+    image: "https://media.giphy.com/media/3o7aD2yQmy5bn4AitO/giphy.gif"
 },  {
     question: "4. Who is Harry Potter's godfather?",
     answers: ["Peter Pettigrew", "Sirius Black", "Remus Lupin", "Severus Snape"],
-    correctAnswer: "Sirius Black"
+    correctAnswer: "Sirius Black",
+    image: "https://media.giphy.com/media/B3z9CYWnCkFRC/giphy.gif"
 }, {
     question: "5. Who put Harry Potter’s name in the Goblet of Fire?",
     answers: ["Mad Eye Moody", "Barty Crouch", "Barty Crouch Jr", "Igor Karkaroff" ],
-    correctAnswer: "Barty Crouch Jr"
+    correctAnswer: "Barty Crouch Jr",
+    image: "https://media.giphy.com/media/zU6duVFqBIzsY/giphy.gif"
 }, {
     question: "6. In the books, who asks Hermione Granger to the Yule Ball?",
     answers: ["Viktor Krum", "Ron Weasley", "Neville Longbottom", "All of the above"],
-    correctAnswer: "All of the above"
+    correctAnswer: "All of the above", 
+    image: "https://media.giphy.com/media/l01fjs9HRzU08/giphy.gif"
 }, {
     question: "7. What form does Harry Potter’s Patronus take?",
     answers: ["Doe", "Dog", "Otter", "Stag"],
-    correctAnswer: "Stag"
+    correctAnswer: "Stag",
+    image: "https://media.giphy.com/media/ru4uZmSQOhatW/giphy.gif"
 }, {
     question: "8. How many Deathly Hollows are there?",
     answers: ["1", "2", "3", "4"],
-    correctAnswer: "3"
+    correctAnswer: "3", 
+    image: "https://media.giphy.com/media/H7qjJSFIvnVBK/giphy.gif"
 }, {
     question: "9. Who killed Albus Dumbledore? ",
     answers: ["Voldemort", "Draco Malfoy", "Harry Potter", "Severus Snape"],
-    correctAnswer: "Severus Snape"
+    correctAnswer: "Severus Snape",
+    image: "https://media.giphy.com/media/XY7LShVWm9xEQ/giphy.gif"
 }, {
-    question: "10. Which of the following professors never taught a Defense -Against the Dark Arts Class?",
+    question: "10. Which of the following professors never taught a Defense Against the Dark Arts Class?",
     answers: ["Severus Snape", "Mad Eye Moody", "Dolores Umbridge", "Gilderoy Lockhart"],
-    correctAnswer: "Mad Eye Moody"
+    correctAnswer: "Mad Eye Moody",
+    image: "https://media.giphy.com/media/S5s0P6MhcLeeY/giphy.gif"
 }]
 
 //when the start button is clicked it kicks off our code.
