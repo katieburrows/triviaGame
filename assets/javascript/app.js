95cbe3b5d243acdb93ba031d02b3ae3225ee75bb
katieburrows <katie_burrows@me.com>
2019-07-06 22:45:57 -0600

    questions and answers iterated through correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -84,8 +84,12 @@ $("#start").on("click", function(event){
 
         var iteratedQuestion = questions[i].question;
 
+        console.log(iteratedQuestion);
         var iteratedAnswers = questions[i].answers;
 
+        for (var j = 0; j < iteratedAnswers.length; j++) {
+            console.log(iteratedAnswers[j]);
+        }
         // //creating true/false radio buttons for each question
         // var radioButtonTrue = $("<fieldset><label class='radio-inline'><input type='radio' value = 'true' name= 'question-" + i +  " '> True </label></fieldset>");
         // var radioButtonFalse = $("<fieldset><label class='radio-inline'><input type='radio' value = 'false' name= 'question-" + i + " '> False</label></fieldset>");
@@ -98,79 +102,79 @@ $("#start").on("click", function(event){
     }
 });
 
-function counter(){
-    //clearing intervalId so that no other instances can affect the timer
-    clearInterval(intervalId);
-    //setting intervalId to run decrement() every second
-    intervalId = setInterval(decrement, 1000);
-}
+// function counter(){
+//     //clearing intervalId so that no other instances can affect the timer
+//     clearInterval(intervalId);
+//     //setting intervalId to run decrement() every second
+//     intervalId = setInterval(decrement, 1000);
+// }
 
 //this function is called every second from counter().
-function decrement() {
-    //taking the time down by one second.
-    timer--;
-    //targeting #countDown div and updating the page with the timer's current position.
-    $("#countDown").html("<h2>" + timer + "</h2>");
-        //conditional to check where the timer is at.  If the timer is run down to 0 we want to end the quiz.
-        if (timer === 0) {
-            //taking the timer off the page.
-            $("#countDown").hide();
+// function decrement() {
+//     //taking the time down by one second.
+//     timer--;
+//     //targeting #countDown div and updating the page with the timer's current position.
+//     $("#countDown").html("<h2>" + timer + "</h2>");
+//         //conditional to check where the timer is at.  If the timer is run down to 0 we want to end the quiz.
+//         if (timer === 0) {
+//             //taking the timer off the page.
+//             $("#countDown").hide();
 
-            $("#gameArea").hide();
+//             $("#gameArea").hide();
             
-            //triggering the function that will check the answers.
-            calculateScore();
-        }
-}
+//             //triggering the function that will check the answers.
+//             calculateScore();
+//         }
+// }
 
-if (timer === 0) {
-    //taking the timer off the page.
-    $("#countDown").hide();
+// if (timer === 0) {
+//     //taking the timer off the page.
+//     $("#countDown").hide();
 
-    $("#gameArea").hide();
+//     $("#gameArea").hide();
     
-    //triggering the function that will check the answers.
-    calculateScore();
-}
+//     //triggering the function that will check the answers.
+//     calculateScore();
+// }
 
-function calculateScore(){
-//thinking that $(this).attr() will be used and looped through again to check against the grading key 
+// // function calculateScore(){
+// //thinking that $(this).attr() will be used and looped through again to check against the grading key 
 
-            //if value grab the value and store it in userGuess
-            //grab value of radio button
-            //compare userGuess to questions[i].answers
+//             //if value grab the value and store it in userGuess
+//             //grab value of radio button
+//             //compare userGuess to questions[i].answers
 
-//two ways to go about this potentially:  create a userGuess variable that will be able to tell if there is no value stored in it (not sure if this is possible with forms, probably is with :checked) OR if there is a true/false value in it.  Use that in the conditionals, if that doesn't work then may need to a create a variable once inside the conditional section that grabs the value (so 2-3 instances of this variable to be created/mutated).
+// //two ways to go about this potentially:  create a userGuess variable that will be able to tell if there is no value stored in it (not sure if this is possible with forms, probably is with :checked) OR if there is a true/false value in it.  Use that in the conditionals, if that doesn't work then may need to a create a variable once inside the conditional section that grabs the value (so 2-3 instances of this variable to be created/mutated).
     
-    for (var i = 0; i < questions[i].answers.length; i++){
-        var radioValue = $(this).attr();
-        console.log(radioValue);
-    }
-        // var radioValue = $("input[name='question-" + [i] + "']:checked").val();
-        // if(radioValue){
-        //     console.log(`${"radio value:"} ${radioValue}`);
-        // }
+//     for (var i = 0; i < questions[i].answers.length; i++){
+//         var radioValue = $(this).attr();
+//         console.log(radioValue);
+//     }
+//         // var radioValue = $("input[name='question-" + [i] + "']:checked").val();
+//         // if(radioValue){
+//         //     console.log(`${"radio value:"} ${radioValue}`);
+//         // }
   
     
 
-    //check if there was an answer; compare userGuess of radio button to correct answer to calculate the score.
-        /*if (!value){
-            noAnswer++
+//     //check if there was an answer; compare userGuess of radio button to correct answer to calculate the score.
+//         /*if (!value){
+//             noAnswer++
 
-        } else if (value){
-            if (value === correctAnswer){
-                right++;
-            } else if (value != correctAnswer){
-                wrong++;
-            }
-        }
+//         } else if (value){
+//             if (value === correctAnswer){
+//                 right++;
+//             } else if (value != correctAnswer){
+//                 wrong++;
+//             }
+//         }
 
-        $("#scoreBoard").append(`${"Score: "}${right}${"/10"}`);
-        $("#scoreBoard").append(`${"Number of wrong answers:"} ${wrong}`);
-        $("#scoreBoard").append(`${"Number of unanswered questions:"} ${noAnswer}`);
-        */
+//         $("#scoreBoard").append(`${"Score: "}${right}${"/10"}`);
+//         $("#scoreBoard").append(`${"Number of wrong answers:"} ${wrong}`);
+//         $("#scoreBoard").append(`${"Number of unanswered questions:"} ${noAnswer}`);
+//         */
         
-}
+// }
 
 //once the timer is done OR the user has hit a submit button the screen is wiped away and the score is reported
 
